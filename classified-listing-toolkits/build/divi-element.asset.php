c74a29321275267aa9dd7c798114d13de5499f3a
Sean Murphy <sean@iamseanmurphy.com>
2025-06-21 21:17:14 +0000

    1.1.3

---
+++ classified-listing-toolkits/build/divi-element.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react'), 'version' => '6548aa14a2149509247b');
---
+++ classified-listing-toolkits/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'radiustheme/classified-listing-toolkits',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '9c77142f60afef58fca409c98b58dd57da978962',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'radiustheme/classified-listing-toolkits' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '9c77142f60afef58fca409c98b58dd57da978962',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
